afea6b0a8e659183e88a4626e1bdf8e7c3a19a84
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-03-09 23:59:40 +0300

    server/http: improve tests
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- server/http/http_test.go
+++ server/http/http_test.go
@@ -253,12 +253,15 @@ func TestNativeServer(t *testing.T) {
 
 }
 
-func TestHTTPServer(t *testing.T) {
+func TestHTTPHandler(t *testing.T) {
 	reg := register.NewRegister()
 	ctx := context.Background()
 
 	// create server
-	srv := httpsrv.NewServer(server.Register(reg))
+	srv := httpsrv.NewServer(
+		server.Register(reg),
+		server.Codec("application/json", jsoncodec.NewCodec()),
+	)
 
 	// create server mux
 	mux := http.NewServeMux()
@@ -271,6 +274,68 @@ func TestHTTPServer(t *testing.T) {
 
 	// register handler
 	if err := srv.Handle(hd); err != nil {
+		t.Fatal(err)
+	}
+
+	// start server
+	if err := srv.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	// lookup server
+	service, err := reg.LookupService(ctx, server.DefaultName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(service) != 1 {
+		t.Fatalf("Expected 1 service got %d: %+v", len(service), service)
+	}
+
+	if len(service[0].Nodes) != 1 {
+		t.Fatalf("Expected 1 node got %d: %+v", len(service[0].Nodes), service[0].Nodes)
+	}
+
+	// make request
+	rsp, err := http.Get(fmt.Sprintf("http://%s", service[0].Nodes[0].Address))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rsp.Body.Close()
+
+	b, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if s := string(b); s != "hello world" {
+		t.Fatalf("Expected response %s, got %s", "hello world", s)
+	}
+
+	// stop server
+	if err := srv.Stop(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestHTTPServer(t *testing.T) {
+	reg := register.NewRegister()
+	ctx := context.Background()
+
+	// create server mux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`hello world`))
+	})
+
+	// create server
+	srv := httpsrv.NewServer(
+		server.Register(reg),
+		httpsrv.Server(&http.Server{Handler: mux}),
+		server.Codec("application/json", jsoncodec.NewCodec()),
+	)
+
+	if err := srv.Init(); err != nil {
 		t.Fatal(err)
 	}
 
