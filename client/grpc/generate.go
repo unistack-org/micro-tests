c0b1e8e375de10257088a09a0b9ed9d5397b555a
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-05-08 12:50:49 +0300

    cleanup stale file
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- client/grpc/generate.go
+++ client/grpc/generate.go
@@ -1,3 +1,3 @@
 package grpc
 
-//go:generate protoc -I./proto -I. --go-grpc_out=paths=source_relative:./proto --go_out=paths=source_relative:./proto --micro_out=standalone=true,paths=source_relative:./gproto proto/test.proto
+//go:generate protoc -I./proto -I. --go-grpc_out=paths=source_relative:./proto --go_out=paths=source_relative:./proto --micro_out=components=micro|rpc,standalone=true,paths=source_relative:./gproto proto/test.proto
