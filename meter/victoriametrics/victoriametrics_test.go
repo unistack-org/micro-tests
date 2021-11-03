f3b7043d5a420b5cb7ba7e64ced4562f040617e6
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-11-03 18:05:16 +0300

    add meter tests
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ meter/victoriametrics/victoriametrics_test.go
@@ -0,0 +1,40 @@
+package victoriametrics_test
+
+import (
+	"bytes"
+	"context"
+	"testing"
+
+	"go.unistack.org/micro/v3/client"
+	"go.unistack.org/micro/v3/codec"
+	"go.unistack.org/micro/v3/meter"
+	"go.unistack.org/micro/v3/meter/wrapper"
+	victoriametrics "go.unistack.org/micro-meter-victoriametrics/v3"
+)
+
+func TestWrapper(t *testing.T) {
+	m := victoriametrics.NewMeter() // meter.Labels("test_key", "test_val"))
+
+	w := wrapper.NewClientWrapper(
+		wrapper.ServiceName("svc1"),
+		wrapper.ServiceVersion("0.0.1"),
+		wrapper.ServiceID("12345"),
+		wrapper.Meter(m),
+	)
+
+	ctx := context.Background()
+
+	c := client.NewClient(client.Wrap(w))
+	if err := c.Init(); err != nil {
+		t.Fatal(err)
+	}
+	rsp := &codec.Frame{}
+	req := &codec.Frame{}
+	err := c.Call(ctx, c.NewRequest("svc2", "Service.Method", req), rsp)
+	_, _ = rsp, err
+	buf := bytes.NewBuffer(nil)
+	_ = m.Write(buf, meter.WriteProcessMetrics(false), meter.WriteFDMetrics(false))
+	if !bytes.Contains(buf.Bytes(), []byte(`micro_client_request_inflight{micro_endpoint="svc2.Service.Method"} 0`)) {
+		t.Fatalf("invalid metrics output: %s", buf.Bytes())
+	}
+}
